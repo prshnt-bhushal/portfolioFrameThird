0ff2b23e3d461c7ab52ea4929361d7606f88f613
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2024-03-06 21:10:47 +0545

    structure of home and about completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import profileImage from '../assets/images/profileImage.png';
 function About() {
   return (
     <section id="about" className="sectionContainer">
-      <h2>About</h2>
-      <p>My name is John Doe and I am a web developer</p>
+      <div className="aboutContainer">
+        <div className="aboutContent">
+          <h2>About Me</h2>
+          <p>
+            My name is Sudeep Shrestha and I am a passionate Computer Engineer.
+            I love creating beautiful and functional websites that make a
+            difference. With a strong background in front-end development and a
+            keen eye for design, I strive to deliver exceptional user
+            experiences.
+          </p>
+          <p>
+            When I'm not coding, you can find me exploring new technologies,
+            reading, or enjoying outdoor activities.
+          </p>
+        </div>
+        <div className="profileImageContainer">
+          <img src={profileImage} alt="Profile" className="profileImage" />
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import profileImage from '../assets/images/profileImage.png';
+import { FaGithub, FaInstagram, FaSquareXTwitter } from 'react-icons/fa6';
+import { FaFileDownload, FaLinkedin } from 'react-icons/fa';
 
 function Home() {
   return (
     <section id="home" className="sectionContainer">
-      <h2>Home</h2>
-      <p>Welcome to my portfolio website</p>
+      <div className="profileContainer">
+        <img src={profileImage} alt="Profile" className="profileImage" />
+        <div className="profileDetails">
+          <h2>Hello, I'm Sudeep Shrestha</h2>
+          <p>Computer Engineer | Mobile App Developer</p>
+          <p>Welcome to my portfolio website</p>
+          <div className="socialHandles">
+            <a href="https://example.com">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="https://example.com">
+              <FaGithub size={24} />
+            </a>
+            <a href="https://example.com">
+              <FaInstagram size={24} />
+            </a>
+            <a href="https://example.com">
+              <FaSquareXTwitter size={24} />
+            </a>
+          </div>
+          <a href="/path/to/your/cv.pdf" download className="cvButton">
+            <span className="dowloadText">Download CV</span>
+            <FaFileDownload className="FaFileDownload" size={24} />
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
